ef9de4697abbcd60f54066efaf86cd9cb0e49cff
AJ Speller <ajspeller@gmail.com>
2020-08-10 01:52:28 -0400

    Component html markup and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
-  declarations: [AppComponent, MainPageComponent],
+  declarations: [AppComponent, MainPageComponent, AddItemFormComponent, BudgetItemListComponent, BudgetItemCardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
     MatDialogModule,
   ],
   providers: [],
